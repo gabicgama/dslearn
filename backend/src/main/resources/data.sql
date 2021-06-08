d89b062c39c673b996eb41ef8d1a129eab402968
Gabriela Gama <gabycgama@hotmail.com>
2021-06-08 11:47:09 -0300

    Course

--- backend/src/main/resources/data.sql
+++ backend/src/main/resources/data.sql
@@ -12,3 +12,5 @@ INSERT INTO tb_user_role (user_id, role_id) VALUES (2, 2);
 INSERT INTO tb_user_role (user_id, role_id) VALUES (3, 1);
 INSERT INTO tb_user_role (user_id, role_id) VALUES (3, 2);
 INSERT INTO tb_user_role (user_id, role_id) VALUES (3, 3);
+
+INSERT INTO tb_course (name, img_Uri, img_Gray_Uri) VALUES ('Bootcamp HTML', 'https://blog.raleduc.com.br/wp-content/uploads/2016/06/aaasd.jpg', 'https://cdn.pixabay.com/photo/2017/06/21/10/50/pay-2426844_1280.png');
