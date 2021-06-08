19f22ca0b453b7772035e7b6096a9db8e55cfeb9
Gabriela Gama <gabycgama@hotmail.com>
2021-06-08 14:37:08 -0300

    Offer, Notification

--- backend/src/main/resources/data.sql
+++ backend/src/main/resources/data.sql
@@ -14,3 +14,8 @@ INSERT INTO tb_user_role (user_id, role_id) VALUES (3, 2);
 INSERT INTO tb_user_role (user_id, role_id) VALUES (3, 3);
 
 INSERT INTO tb_course (name, img_Uri, img_Gray_Uri) VALUES ('Bootcamp HTML', 'https://blog.raleduc.com.br/wp-content/uploads/2016/06/aaasd.jpg', 'https://cdn.pixabay.com/photo/2017/06/21/10/50/pay-2426844_1280.png');
+
+INSERT INTO tb_offer (edition, start_Moment, end_Moment, course_id) VALUES ('1.0', TIMESTAMP WITH TIME ZONE '2020-11-20T03:00:00Z', TIMESTAMP WITH TIME ZONE '2021-11-20T03:00:00Z', 1);
+INSERT INTO tb_offer (edition, start_Moment, end_Moment, course_id) VALUES ('2.0', TIMESTAMP WITH TIME ZONE '2020-12-20T03:00:00Z', TIMESTAMP WITH TIME ZONE '2021-12-20T03:00:00Z', 1);
+
+INSERT INTO tb_notification (text, moment, read, route, user_id) VALUES ('Mensagem de teste', TIMESTAMP WITH TIME ZONE '2020-06-08T17:32:00Z', True, '/users/1', 1);
