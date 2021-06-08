0f1b48b33f53f87b8b48f408f8efecf5ee696cdf
Gabriela Gama <gabycgama@hotmail.com>
2021-06-08 15:00:18 -0300

    Resource

--- backend/src/main/resources/data.sql
+++ backend/src/main/resources/data.sql
@@ -19,3 +19,5 @@ INSERT INTO tb_offer (edition, start_Moment, end_Moment, course_id) VALUES ('1.0
 INSERT INTO tb_offer (edition, start_Moment, end_Moment, course_id) VALUES ('2.0', TIMESTAMP WITH TIME ZONE '2020-12-20T03:00:00Z', TIMESTAMP WITH TIME ZONE '2021-12-20T03:00:00Z', 1);
 
 INSERT INTO tb_notification (text, moment, read, route, user_id) VALUES ('Mensagem de teste', TIMESTAMP WITH TIME ZONE '2020-06-08T17:32:00Z', True, '/users/1', 1);
+
+INSERT INTO tb_resource (title, description, position, img_Uri, type, offer_id) VALUES ('Trilha HTML', 'Trilha principal do curso', 1, 'https://blog.raleduc.com.br/wp-content/uploads/2016/06/aaasd.jpg', 2, 1);
