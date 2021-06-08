9a00123de23d3d7e8a93dc4041f9e03d5d046660
Gabriela Gama <gabycgama@hotmail.com>
2021-06-08 16:02:16 -0300

    Section

--- backend/src/main/resources/data.sql
+++ backend/src/main/resources/data.sql
@@ -21,3 +21,9 @@ INSERT INTO tb_offer (edition, start_Moment, end_Moment, course_id) VALUES ('2.0
 INSERT INTO tb_notification (text, moment, read, route, user_id) VALUES ('Mensagem de teste', TIMESTAMP WITH TIME ZONE '2020-06-08T17:32:00Z', True, '/users/1', 1);
 
 INSERT INTO tb_resource (title, description, position, img_Uri, type, offer_id) VALUES ('Trilha HTML', 'Trilha principal do curso', 1, 'https://blog.raleduc.com.br/wp-content/uploads/2016/06/aaasd.jpg', 2, 1);
+INSERT INTO tb_resource (title, description, position, img_Uri, type, offer_id) VALUES ('Trilha CSS', 'Trilha secundária do curso', 2, 'https://blog.raleduc.com.br/wp-content/uploads/2016/06/aaasd.jpg', 2, 1);
+
+INSERT INTO tb_section (title, description, position, img_Uri, resource_id, prerequisite_id) VALUES ('Capítulo 1', 'Neste capítulo vamos começar!', 1, 'https://blog.raleduc.com.br/wp-content/uploads/2016/06/aaasd.jpg', 1, null);
+INSERT INTO tb_section (title, description, position, img_Uri, resource_id, prerequisite_id) VALUES ('Capítulo 2', 'Neste capítulo vamos começar!', 2, 'https://blog.raleduc.com.br/wp-content/uploads/2016/06/aaasd.jpg', 2, 1);
+
+
