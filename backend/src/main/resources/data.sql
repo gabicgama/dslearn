68871c589bf335860141f29e5893ed805a872941
Gabriela Gama <gabycgama@hotmail.com>
2021-06-08 21:56:25 -0300

    Lesson seed, fix lessonsDone

--- backend/src/main/resources/data.sql
+++ backend/src/main/resources/data.sql
@@ -29,3 +29,14 @@ INSERT INTO tb_section (title, description, position, img_Uri, resource_id, prer
 INSERT INTO tb_enrollment (user_id, offer_id, enroll_Moment, refund_Moment, available, only_Update) VALUES (1, 1, TIMESTAMP WITH TIME ZONE '2021-11-20T13:00:00Z', null, true, false);
 INSERT INTO tb_enrollment (user_id, offer_id, enroll_Moment, refund_Moment, available, only_Update) VALUES (2, 1, TIMESTAMP WITH TIME ZONE '2021-11-20T13:00:00Z', null, true, false);
 
+INSERT INTO tb_lesson (title, position, section_id) VALUES ('Aula 1 - Capítulo 1', 1, 1);
+INSERT INTO tb_content (text_Content, video_Uri, id) VALUES ('Material de apoio', 'https://www.youtube.com/watch?v=PfYifUFmXk8', 1);
+
+INSERT INTO tb_lesson (title, position, section_id) VALUES ('Aula 2 - Capítulo 1', 2, 1);
+INSERT INTO tb_content (text_Content, video_Uri, id) VALUES ('Material de apoio', 'https://www.youtube.com/watch?v=PfYifUFmXk8', 2);
+
+INSERT INTO tb_lesson (title, position, section_id) VALUES ('Tarefa - Capítulo 1', 3, 1);
+INSERT INTO tb_task (description, question_Count, approval_Count, weight, due_Date, id) VALUES ('Fazer o desafio', 5, 4, 1.0, TIMESTAMP WITH TIME ZONE '2021-11-20T13:00:00Z', 3);
+
+INSERT INTO tb_lessons_done (lesson_id, user_id, offer_id) VALUES (1, 1, 1);
+INSERT INTO tb_lessons_done (lesson_id, user_id, offer_id) VALUES (2, 1, 1);
